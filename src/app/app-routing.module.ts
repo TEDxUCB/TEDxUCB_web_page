1043018df74373d45c6ceefc59df2cddaac35264
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-07-18 01:51:33 -0400

    created Routing for Eventitem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { EventComponent } from './pages/event/event.component';
+import { EventItemComponent } from './pages/event-item/event-item.component';
 
 
 const routes: Routes = [
   {path : 'Home', component: HomeComponent},
   {path : 'Events', component: EventComponent},
+  {path : 'EventsItem', component: EventItemComponent},
   {path : '**',pathMatch:'full', redirectTo: 'Home'},
   
 ];
