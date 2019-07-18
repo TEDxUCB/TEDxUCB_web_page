5689d1e125d65b2c14d587b1751ffd3ea6992900
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-07-18 01:58:36 -0400

    created Routing for Gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { EventComponent } from './pages/event/event.component';
 import { EventItemComponent } from './pages/event-item/event-item.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 
 const routes: Routes = [
   {path : 'Home', component: HomeComponent},
   {path : 'Events', component: EventComponent},
   {path : 'EventsItem', component: EventItemComponent},
+  {path : 'Gallery', component: GalleryComponent},
   {path : '**',pathMatch:'full', redirectTo: 'Home'},
   
 ];
