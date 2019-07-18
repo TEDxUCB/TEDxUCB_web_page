514ff433d6f838e6e3b23a23f0f51881e92db243
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-07-18 02:07:43 -0400

    created Routing for About

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { EventComponent } from './pages/event/event.component';
 import { EventItemComponent } from './pages/event-item/event-item.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { TeamComponent } from './pages/team/team.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path : 'EventsItem', component: EventItemComponent},
   {path : 'Gallery', component: GalleryComponent},
   {path : 'Team', component: TeamComponent},
+  {path : 'About', component: AboutComponent},
   {path : '**',pathMatch:'full', redirectTo: 'Home'},
   
 ];
