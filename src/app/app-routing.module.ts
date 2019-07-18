5491b5ba383b25febefe4b477de6862d8d2f7ce9
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-07-18 01:40:27 -0400

    Routing module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EventComponent } from './pages/event/event.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'Home', component: HomeComponent},
+  {path : 'Events', component: EventComponent},
+  {path : '**',pathMatch:'full', redirectTo: 'Home'},
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { EventComponent } from './pages/event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
